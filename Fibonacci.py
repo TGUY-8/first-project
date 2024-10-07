38d4e8c5ace5edc9434110bb9e0ff0ea3d01c067
TGUY-8 <luxiran816@gmail.com>
2024-10-07 10:19:27 +0800

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -9,9 +9,12 @@ def add_(n):
     else:
         print("POSITIVE INTEGER ONLY!")
         return False
-try:
-    x=int(input("Calculate the n-th value of the Fibonacci sequence,n="))
-    k=add_(x)
-    print("The value is",k)
-except ValueError:
-    print("Error")
+def main():
+    try:
+        x=int(input("Calculate the n-th value of the Fibonacci sequence,n="))
+        k=add_(x)
+        print("The value is",k)
+    except ValueError:
+        print("Error")
+if __name__=='__main__':
+    main()
